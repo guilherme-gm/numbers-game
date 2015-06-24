728ad4de6e7385d46f5397fa39f5bad27223b475
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-06-24 00:10:53 -0300

    - Reworked product classes - Added Click Handling for Buy - Added money update

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,31 +1,71 @@
 ﻿using System.Collections;
 
 public enum Products {
-	Prod1,
-	Prod2
+	Prod1 = 1,
+	Prod2 = 2,
 }
 
-public class Product
+public abstract class Product
 {
-	public BigInteger NextLevelPrice;
-	public int Level;
+	public int Level { get; private set; }
 
-	public Product(Products type, int level = 0)
+	public BigInteger NextLevelPrice { get; private set; }
+	public BigInteger Income { get; private set; }
+
+	public Product(int level = 0)
 	{
-		switch (type)
-		{
-
-		case Products.Prod1:
-			// Price formula
-			NextLevelPrice = 10 + 10*level;
-			break;
-
-		case Products.Prod2:
-			NextLevelPrice = 50 + 50*level;
-			break;
-
-		}
+		this.Level = level;
+		this.UpdateIncome ();
+		this.UpdateLevelPrice ();
 	}
 
+	public void Upgrade()
+	{
+		this.Level++;
+		this.UpdateIncome ();
+		this.UpdateLevelPrice ();
+	}
 
+	public abstract void UpdateIncome ();
+	public abstract void UpdateLevelPrice ();
+
+	public void UpdateIncome(BigInteger val) {
+		this.Income = val;
+	}
+
+	public void UpdateLevelPrice(BigInteger val) {
+		this.NextLevelPrice = val;
+	}
+}
+
+public class Prod1 : Product
+{
+	public Prod1 (int level) : base(level){}
+
+	public override void UpdateIncome ()
+	{
+		base.UpdateIncome (10 * base.Level);
+	}
+
+	public override void UpdateLevelPrice ()
+	{
+		base.UpdateLevelPrice (10 + 20 * base.Level);
+	}
+
+}
+
+public class Prod2 : Product
+{
+	public Prod2 (int level) : base(level){}
+	
+	public override void UpdateIncome ()
+	{
+		base.UpdateIncome (50 * base.Level);
+	}
+	
+	public override void UpdateLevelPrice ()
+	{
+		base.UpdateLevelPrice (50 + 100 * base.Level);
+	}
+	
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	Dictionary<Products, Product> ProdList;
+	public Dictionary<Products, Product> ProdList { get; private set; }
+	public BigInteger Money { get; private set; }
+	public Text[] ProdButtons;
+	public Text MoneyDisplay;
+
+	private float NextUpdate = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 		this.ProdList = new Dictionary<Products, Product> ();
 
-		ProdList.Add (Products.Prod1, new Product (Products.Prod1, 1));
-		ProdList.Add (Products.Prod2, new Product (Products.Prod2, 0));
+		ProdList.Add (Products.Prod1, new Prod1(1));
+		ProdList.Add (Products.Prod2, new Prod2(0));
+
+		foreach (Products p in ProdList.Keys) {
+			UpdateButtonDisplay(p);
+		}
+
+		Money = 0;
+		NextUpdate = Time.time;
 	}
 
 	/// <summary>
@@ -22,5 +35,41 @@ public class GameController : MonoBehaviour {
 	{
 		// Converts buttonId to enum
 		Products p = (Products)buttonId;
+
+		// Ensures that this Id exists
+		if (!ProdList.ContainsKey (p)) {
+			// TODO : Error handling.
+			return;
+		}
+
+		if (this.Money >= ProdList [p].NextLevelPrice) {
+			Money -= ProdList[p].NextLevelPrice;
+			UpdateMoneyDisplay();
+			ProdList [p].Upgrade ();
+			UpdateButtonDisplay(p);
+		}
+	}
+
+	private void Update()
+	{
+		if (Time.time > this.NextUpdate) {
+			foreach(Product prod in ProdList.Values)
+			{
+				Money +=  prod.Income;
+			}
+
+			this.UpdateMoneyDisplay();
+			this.NextUpdate += 1.0f;
+		}
+	}
+
+	private void UpdateMoneyDisplay()
+	{
+		MoneyDisplay.text = this.Money.ToString ();
+	}
+
+	private void UpdateButtonDisplay(Products type)
+	{
+		ProdButtons [(int)type].text = "Lv " + ProdList[type].Level + " - " + ProdList [type].NextLevelPrice;
 	}
 }
