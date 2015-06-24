04b401acf29c0c05634ab29db1259c7f3de00044
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-06-23 23:20:48 -0300

    - Added Button and Background textures - Start UI - Added BigInteger and Data classes - Initialized click handling

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+public enum Products {
+	Prod1,
+	Prod2
+}
+
+public class Product
+{
+	public BigInteger NextLevelPrice;
+	public int Level;
+
+	public Product(Products type, int level = 0)
+	{
+		switch (type)
+		{
+
+		case Products.Prod1:
+			// Price formula
+			NextLevelPrice = 10 + 10*level;
+			break;
+
+		case Products.Prod2:
+			NextLevelPrice = 50 + 50*level;
+			break;
+
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	Dictionary<Products, Product> ProdList;
+
+	// Use this for initialization
+	void Start () {
+		this.ProdList = new Dictionary<Products, Product> ();
+
+		ProdList.Add (Products.Prod1, new Product (Products.Prod1, 1));
+		ProdList.Add (Products.Prod2, new Product (Products.Prod2, 0));
+	}
+
+	/// <summary>
+	/// Called whenever one of products buy button is clicked.
+	/// </summary>
+	/// <param name="buttonId">Button identifier.</param>
+	public void OnBuyClick(int buttonId)
+	{
+		// Converts buttonId to enum
+		Products p = (Products)buttonId;
+	}
+}
