513256d264ee145fa6ccc2f6fe222b516b8aa239
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-06-29 20:53:51 -0300

    Added UpgradeButton Generator and Prefab

---
+++ Assets/Scripts/UpgradeButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[System.Serializable]
+public class UpgradeBtnData {
+	public int UpgradeId { get; set; }
+	public string Title { get; set; }
+	public string Description { get; set; }
+	public BigInteger Price { get; set; }
+}
+
+public class UpgradeButton : MonoBehaviour {
+	public UpgradeBtnData Data;
+
+	public Text TitleText;
+	public Text DescText;
+	public Text PriceText;
+	
+	void Start()
+	{
+		this.TitleText.text = Data.Title;
+		this.DescText.text = Data.Description;
+		this.PriceText.text = "" + Data.Price;
+	}
+
+	public void OnClickEvent()
+	{
+
+	}
+}
